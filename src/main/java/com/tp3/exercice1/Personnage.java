f101c64dc4e88f6c7fb3e76d27967c1bc28d5ba1
User <aitbouzianibtissam@gmail.com>
2024-12-19 16:13:59 +0100

    2 cycle EST

--- src/main/java/com/tp3/exercice1/Personnage.java
+++ src/main/java/com/tp3/exercice1/Personnage.java
@@ -3,6 +3,10 @@ package com.tp3.exercice1;
 public class Personnage {
     public static String tourner(int nbr){
         String ch="NORD";
+        if(nbr%4==1){
+            ch="EST";
+            return ch;
+        }
         return ch;
     }
 }
--- src/test/java/com/tp3/exercice3/PersonnageTest.java
+++ src/test/java/com/tp3/exercice3/PersonnageTest.java
@@ -7,9 +7,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PersonnageTest {
     @Test
-    void tourn_test(){
+    void tourn_test0(){
         Personnage P= new Personnage();
         String result=P.tourner(0);
         assertEquals("NORD",result,"we be "+"NORD"+" but we "+result);
+    }
+    @Test
+    void tourn_test1(){
+        Personnage P= new Personnage();
+        String result=P.tourner(1);
+        assertEquals("EST",result,"we be "+"EST"+" but we "+result);
     }
 }
